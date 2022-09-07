d14fcc41236a2eff729b2fd30e2e0b2a829f90dd
labalabadie <labalabadie@gmail.com>
2022-09-07 11:21:02 -0700

    2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,9 +12,23 @@ if __name__ == "__main__":
                      format(argv[1])).json()
     user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
                              format(argv[1])).json()
+    completed_tasks = 0
+    uncompleted_tasks = 0
+    for elem in j:
+        if elem['completed'] is True:
+            completed_tasks = completed_tasks + 1
+    for elem in j:
+        if elem['completed'] is False:
+            uncompleted_tasks = uncompleted_tasks + 1
+    print("Employee {} is done with tasks ({}/{}):".
+          format(user_name['name'], completed_tasks,
+                 (completed_tasks + uncompleted_tasks)))
+    for elem in j:
+        if elem['completed'] is True:
+            print("\t {}".format(elem['title']))
     with open('2.csv', mode='a', encoding='utf-8') as csv_file:
         for elem in j:
             a = '"{}","{}","{}","{}"\n'.format(argv[1], user_name['username'],
-                                               elem['completed'],
+     	                                       elem['completed'],
                                                elem['title'])
-            csv_file.write(a)
+        csv_file.write(a)
