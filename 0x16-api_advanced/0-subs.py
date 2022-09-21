86862abff27ce6225060f4ae164b79e8c10452b9
labalabadie <labalabadie@gmail.com>
2022-09-21 09:45:02 -0700

    new status code check with try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,10 @@ def number_of_subscribers(subreddit):
     """Returns number of subscribers in a given subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "Mozilla/5.0"})
-
-    return(sub_info.json()["data"]["subscribers"])
+    try:
+        return(response.json()["data"]["subscribers"])
+    except Exception:
+        return 0
